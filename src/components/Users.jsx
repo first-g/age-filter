d3bd50e0e567af5d3827e5c474afe214bed27743
NuremirKadyrbekov <118036760+NuremirKadyrbekov@users.noreply.github.com>
2023-05-15 21:53:38 +0600

    the task ready

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Css from './User.css'
 const Users = ({name, age, country}) => {
     return (
         <div>
-            <h2>{name}</h2>
+            <h2 className={Css.UserName}>{name}</h2>
             <p>{age}</p>
             <p>{country}</p>
         </div>
