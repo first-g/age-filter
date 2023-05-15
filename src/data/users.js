d41665c4c643ac89c44df1aef9110387c2bcd35c
first-g <gamerfreestyle14@gmail.com>
2023-05-15 19:17:41 +0600

    upd users

--- src/data/users.js
+++ src/data/users.js
@@ -1,17 +1,17 @@
 // кому меньше 
 let tinagers = [
-    {name: "Tunuk", age: "18", country: "Kyrgyzstan"},
-    {name: "Sezim", age: "17", country: "Kazakhstan"},
-    {name: "Nazmi", age: "16", country: "Uzbekistan"},
-    {name: "Diana", age: "15", country: "Kyrgyzstan"},
-    {name: "Adelya", age: "18", country: "Germany"},
+    {name: "Tunuk", age: 18, country: "Kyrgyzstan"},
+    {name: "Sezim", age: 17, country: "Kazakhstan"},
+    {name: "Nazmi", age: 16, country: "Uzbekistan"},
+    {name: "Diana", age: 15, country: "Kyrgyzstan"},
+    {name: "Adelya", age: 18, country: "Germany"},
 ]
 
 // кому больше
 let adults = [
-    {name: "albina", age: "20", country: "Kyrgyzstan"},
-    {name: "Azimbek", age: "21", country: "Kazakhstan"},
-    {name: "Erbol", age: "22", country: "Uzbekistan"},
-    {name: "Nuremir", age: "24", country: "Kyrgyzstan"},
-    {name: "Bekjan", age: "26", country: "Germany"},
+    {name: "albina", age: 20, country: "Kyrgyzstan"},
+    {name: "Azimbek", age: 21, country: "Kazakhstan"},
+    {name: "Erbol", age: 22, country: "Uzbekistan"},
+    {name: "Nuremir", age: 24, country: "Kyrgyzstan"},
+    {name: "Bekjan", age: 26, country: "Germany"},
 ]
