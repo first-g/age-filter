148e6cf9d32b14c45034b5ad5f5d6a295e013156
NuremirKadyrbekov <118036760+NuremirKadyrbekov@users.noreply.github.com>
2023-05-16 15:18:49 +0600

    the task ready

--- src/components/UsersComponent.jsx
+++ src/components/UsersComponent.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './User.css'
 
 const UsersComponent = ({name, age, country, img}) => {
+     const[Open, SetOpen] = useState()
+     
+        const ShowProf =()=>{
+           return (
+              <div>
+                <img src={img} alt="" />
+                <p>{name}</p>
+                <p>{age}</p>
+                 <p>{country}</p>
+              </div>
+           );
+    }
     return (
-        <div className='user'>
+        <div className='user' onClick={()=>SetOpen(true)}>
             <img src={img} className="imgUser"/>
             <h2 className='name'>имя:{name}</h2>
             <p className='age'>возраст:{age}</p>
             
-            <p className='country'>страна:{country}</p>
+            <p className='country' >страна:{country}</p>
         </div>
     )
 }
--- src/data/users.js
+++ src/data/users.js
@@ -1,11 +1,11 @@
 export const users = [
     {name: "Tunuk", age: 16, country: "Kyrgyzstan", img: "../src/assets/images/p2.png"},
-    {name: "Sezim", age: 17, country: "Kazakhstan", img: "../src/assets/images/p1.jpg"},
+    {name: "Azimbek", age: 17, country: "Kazakhstan", img: "../src/assets/images/p1.png"},
     {name: "Nazmi", age: 16, country: "Uzbekistan", img: "../src/assets/images/p3.png"},
     {name: "Bekjan", age: 26, country: "Germany", img: "../src/assets/images/p4.png"},
     {name: "Adelya", age: 18, country: "Germany", img: "../src/assets/images/p5.png"},
     {name: "albina", age: 20, country: "Kyrgyzstan", img: "../src/assets/images/p6.png"},
-    {name: "Azimbek", age: 21, country: "Kazakhstan", img: "../src/assets/images/p7.png"},
+    {name: "Sezim", age: 21, country: "Kazakhstan", img: "../src/assets/images/p7.png"},
     {name: "Erbol", age: 22, country: "Uzbekistan", img: "../src/assets/images/p8.png"},
     {name: "Nuremir", age: 25, country: "Kyrgyzstan", img: "../src/assets/images/p9.png"},
 ]
