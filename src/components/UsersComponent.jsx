b4cd16d43d7dec34e27ce1d11d1e98c9b19a0301
NuremirKadyrbekov <118036760+NuremirKadyrbekov@users.noreply.github.com>
2023-05-16 15:47:23 +0600

    the task ready

--- src/components/UsersComponent.jsx
+++ src/components/UsersComponent.jsx
@@ -2,20 +2,9 @@ import React, { useState } from 'react'
 import './User.css'
 
 const UsersComponent = ({name, age, country, img}) => {
-     const[Open, SetOpen] = useState()
-     
-        const ShowProf =()=>{
-           return (
-              <div>
-                <img src={img} alt="" />
-                <p>{name}</p>
-                <p>{age}</p>
-                 <p>{country}</p>
-              </div>
-           );
-    }
+    
     return (
-        <div className='user' onClick={()=>SetOpen(true)}>
+        <div className='user' >
             <img src={img} className="imgUser"/>
             <h2 className='name'>имя:{name}</h2>
             <p className='age'>возраст:{age}</p>
